257d9faa0c8371b7c6ad8bbe998f2d3d0fa5bd3a
Kevin Minseok Oh <oms3749@gmail.com>
2022-08-17 15:06:30 +0900

    feat: 빈 문자열 제출 시 에러 스타일 표시

--- src/basic-practice/styling-basic/src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/basic-practice/styling-basic/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,36 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return
+    }
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
   };
+
+  const inValidInputStyle = {
+    borderColor: 'red',
+    background: 'salmon'
+  }
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
