17ada6ec65d8f6dfa0a77c25fa9786030811f898
Kevin Minseok Oh <oms3749@gmail.com>
2022-08-19 23:02:38 +0900

    feat: useEffect를 통한 input handling 추가

--- src/basic-practice/effect-practice/src/App.js
+++ src/basic-practice/effect-practice/src/App.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
+const LOGIN_KEY = 'isLoggedIn';
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const userLoggedInInfo = localStorage.getItem(LOGIN_KEY);
+
+    if (userLoggedInInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, [])
+
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    localStorage.setItem(LOGIN_KEY, '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem(LOGIN_KEY);
     setIsLoggedIn(false);
   };
 
--- src/basic-practice/effect-practice/src/components/Login/Login.js
+++ src/basic-practice/effect-practice/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,20 +11,24 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 150);
+
+    return () => {
+      clearTimeout(identifier);
+    }
+  }, [enteredEmail, enteredPassword]);
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
