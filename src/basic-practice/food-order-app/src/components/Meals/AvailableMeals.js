958303644534628ed5ca9c740fb4d627c7a78959
Kevin Minseok Oh <oms3749@gmail.com>
2022-08-30 18:05:37 +0900

    Create Firebase realtime database url

--- src/basic-practice/food-order-app/src/components/Meals/AvailableMeals.js
+++ src/basic-practice/food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,6 +1,7 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+import { FirebaseURL } from '../../FirebaseURL';
 
 const DUMMY_MEALS = [
   {
